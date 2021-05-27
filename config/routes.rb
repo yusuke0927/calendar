8cfcdd20b1c1d3d307413de6971ba16c057651c3
yusuke0927 <gorirando0927@gmail.com>
2021-05-28 03:42:50 +0900

    モデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  devise_for :users
+  resources :events 
+  root to:'calendars#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
